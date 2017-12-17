8adb5a10d668dd4d98d0101858b574f40d634051
SonaAPI <sonaapi17@gmail.com>
2017-12-17 21:59:56 +0900

    settings 설정중

--- mall/settings.py
+++ mall/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import pymysql
 import os
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'k!0u9gcrw*m_pm_wrmhre+!_hwf*awhe^!3wb_ci+^=!^2+!se'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '.sonaapi.ga']
 
 
 # Application definition
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mall', # DB명
+        'USER': 'mall', # 데이터베이스 계정
+        'PASSWORD': 'mall', # 계정 비밀번호
+        'HOST': '113.198.236.95', # 데이테베이스 주소(IP)
+        'PORT': '3306', # 데이터베이스 포트(보통은 3306)
     }
 }
 
