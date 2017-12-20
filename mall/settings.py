9cf5051a4a51401aa686358a678f1d7c2ddb4c7b
SonaAPI <sonaapi17@gmail.com>
2017-12-21 06:03:43 +0900

    category 페이지 수정

--- mall/settings.py
+++ mall/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-TEMPLATE_DIRS = (
-os.path.join(BASE_DIR, 'templates'),
-)
