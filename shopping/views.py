253f78431094d1821a8996bd2230b6c7892b4ec5
SonaAPI <sonaapi17@gmail.com>
2017-12-21 21:01:10 +0900

    category 페이지 수정

--- shopping/views.py
+++ shopping/views.py
@@ -13,6 +13,26 @@ def photo(request):
 def contacts(request):
     return render(request,'shopping/contacts.html')
 
-
 def category_cpu(request):
     return render(request,'shopping/cpu.html')
+
+def category_ram(request):
+    return render(request,'shopping/ram.html')
+
+def category_mb(request):
+    return render(request,'shopping/mb.html')
+
+def category_vga(request):
+    return render(request,'shopping/vga.html')
+
+def category_ssdhdd(request):
+    return render(request,'shopping/ssdhdd.html')
+
+def category_case(request):
+    return render(request,'shopping/case.html')
+
+def category_power(request):
+    return render(request,'shopping/power.html')
+
+def category_inputdevices(request):
+    return render(request,'shopping/inputdevices.html')
